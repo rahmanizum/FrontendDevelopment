f66b4ae211332d773508b52d65538ba212c8ac22
Mufil Rahman A <ramanizum@gmail.com>
2023-12-28 17:09:37 +0530

    rest and spread

---
+++ jsrefresher/rest.js
@@ -0,0 +1,4 @@
+function sort(...arg){
+    return arg.sort((a,b)=>a-b);
+}
+console.log(sort(3,2,3,4,1,-6));
---
+++ jsrefresher/spread.js
@@ -0,0 +1,5 @@
+function printing(arg){
+    console.log(...arg);
+}
+
+printing([1,2,3,4,5,6])
