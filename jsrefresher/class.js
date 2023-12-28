798ba49e892cd5761d73dcc74e7d0c5ed37d17ee
Mufil Rahman A <ramanizum@gmail.com>
2023-12-28 16:02:26 +0530

    arrow function

--- jsrefresher/class.js
+++ jsrefresher/class.js
@@ -1,23 +1,17 @@
-class Human{
-    constructor(){
-        this.species  = 'Homo Sapiens';
-    }
-    printSpecies(){
-        console.log(`I am a ${this.species}`);
-    }
+class Human {
+  species = "Homo Sapiens";
+  printSpecies = () => {
+    console.log(`I am a ${this.species}`);
+  };
 }
 
-class Male extends Human{
-    constructor(name){
-        super()
-        this.name = name
-        this.gender = 'Male'
-    }
-    printGender(){
-        console.log(`Gender is ${this.gender}`);
-    }
+class Male extends Human {
+  gender = "Male";
+  printGender = ()=> {
+    console.log(`Gender is ${this.gender}`);
+  }
 }
 
-let person1 = new Male("Mufil");
-person1.printSpecies(); 
+let person1 = new Male();
+person1.printSpecies();
 person1.printGender();
