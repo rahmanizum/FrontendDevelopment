8ed1418d62505f55e7243cde2df048583a70df4e
Mufil Rahman A <ramanizum@gmail.com>
2023-12-29 19:49:56 +0530

    back to previous

--- react-app-v1/src/components/Expenses/ExpenseItem.jsx
+++ react-app-v1/src/components/Expenses/ExpenseItem.jsx
@@ -1,14 +1,17 @@
 import React from 'react'
 import ExpenseDate from './ExpenseDate'
 import Card from '../UI/Card'
-import ExpenseDetails from './ExpenseDetails'
 import './ExpenseItem.css'
 
 const ExpenseItem = ({date,title,amount}) => {
   return (
     <Card className='expense-item '>
       <ExpenseDate date={date}/>
-      <ExpenseDetails title={title} amount={amount} />      
+        <div className='expense-item__description'>
+            <h2>{title}</h2>
+            <div className='expense-item__price'>$ {amount}</div>
+        </div>
+      
     </Card>
   )
 }
