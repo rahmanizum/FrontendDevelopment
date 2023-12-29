4c028f8c4d1a79ed115807b7e8dd6d8fabc3e19f
Mufil Rahman A <ramanizum@gmail.com>
2023-12-29 23:05:49 +0530

    use state

--- react-app-v1/src/components/Expenses/ExpenseItem.jsx
+++ react-app-v1/src/components/Expenses/ExpenseItem.jsx
@@ -1,18 +1,21 @@
-import React from 'react'
+import React, {useState } from 'react'
 import ExpenseDate from './ExpenseDate'
 import Card from '../UI/Card'
 import './ExpenseItem.css'
 
 const ExpenseItem = ({date,title,amount}) => {
+  const [data,dataState] = useState(title)
+  const clickHandler = ()=>{
+    dataState("Updated")
+  }
   return (
     <Card className='expense-item '>
       <ExpenseDate date={date}/>
         <div className='expense-item__description'>
-            <h2>{title}</h2>
+            <h2>{data}</h2>
             <div className='expense-item__price'>$ {amount}</div>
         </div>
-        
-      
+        <button onClick={clickHandler}>Change title</button>
     </Card>
   )
 }
