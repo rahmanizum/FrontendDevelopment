e5b8e0cfdafe3ba76d674535e7cd112a384a031f
Mufil Rahman A <ramanizum@gmail.com>
2023-12-29 19:46:38 +0530

    addition of Expense details component

--- react-app-v1/src/components/Expenses/ExpenseItem.jsx
+++ react-app-v1/src/components/Expenses/ExpenseItem.jsx
@@ -1,17 +1,14 @@
 import React from 'react'
-import './ExpenseItem.css'
 import ExpenseDate from './ExpenseDate'
 import Card from '../UI/Card'
+import ExpenseDetails from './ExpenseDetails'
+import './ExpenseItem.css'
 
 const ExpenseItem = ({date,title,amount}) => {
   return (
     <Card className='expense-item '>
       <ExpenseDate date={date}/>
-        <div className='expense-item__description'>
-            <h2>{title}</h2>
-            <div className='expense-item__price'>$ {amount}</div>
-        </div>
-      
+      <ExpenseDetails title={title} amount={amount} />      
     </Card>
   )
 }
