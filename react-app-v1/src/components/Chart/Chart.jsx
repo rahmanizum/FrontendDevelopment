31627f8a3f95030ef139f8c74f22970dc174febe
Mufil Rahman A <ramanizum@gmail.com>
2023-12-31 00:56:39 +0530

    ExpenseChart component

--- react-app-v1/src/components/Chart/Chart.jsx
+++ react-app-v1/src/components/Chart/Chart.jsx
@@ -3,10 +3,14 @@ import ChartBar from "./ChartBar";
 import "./Chart.css";
 
 const Chart = ({ dataPoints }) => {
+  let dataPointsValues = dataPoints.map((ele)=>{
+    ele.value;
+  })
+  let totalMax = Math.max(...dataPointsValues)
   return (
     <div className="chart">
       {dataPoints.map((ele) => {
-        return <ChartBar key={ele.label} {...ele} maxValue={null} />;
+        return <ChartBar key={ele.label} {...ele} maxValue={totalMax} />;
       })}
     </div>
   );
