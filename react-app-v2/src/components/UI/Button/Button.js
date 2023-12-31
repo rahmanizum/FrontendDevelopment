65157c8d3cc85b467da81bf241dc66a3d7d4036d
Mufil Rahman A <ramanizum@gmail.com>
2023-12-31 23:24:28 +0530

    adding media query

--- react-app-v2/src/components/UI/Button/Button.js
+++ react-app-v2/src/components/UI/Button/Button.js
@@ -1,8 +1,8 @@
-
 import styled from "styled-components";
 
 const Button = styled.button`
-  font: inherit;
+  width: 100%;
+  font: inherit; 
   padding: 0.5rem 1.5rem;
   border: 1px solid #8b005d;
   color: white;
@@ -10,6 +10,10 @@ const Button = styled.button`
   box-shadow: 0 0 4px rgba(0, 0, 0, 0.26);
   cursor: pointer;
 
+  @media (min-width: 768px) {
+    width: auto;
+  }
+  
   &:focus {
     outline: none;
   }
