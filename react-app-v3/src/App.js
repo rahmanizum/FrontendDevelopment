e0535ba8150dd812ae5834ea1aa40c8878dd2479
Mufil Rahman A <ramanizum@gmail.com>
2024-01-01 18:24:08 +0530

    Manage User project setup

---
+++ react-app-v3/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import './App.css';
+
+const App = () => {
+  return (
+    <div>
+    <h3>Hello World</h3>
+    </div>
+  );
+};
+
+export default App;
