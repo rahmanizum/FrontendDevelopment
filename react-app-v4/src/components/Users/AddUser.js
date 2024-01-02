c79f30aac9e47a03705cd950409d32fd8b5022e6
Mufil Rahman A <ramanizum@gmail.com>
2024-01-02 15:45:45 +0530

    inbuild wrapper syntax

--- react-app-v4/src/components/Users/AddUser.js
+++ react-app-v4/src/components/Users/AddUser.js
@@ -4,7 +4,7 @@ import Card from '../UI/Card';
 import Button from '../UI/Button';
 import ErrorModal from '../UI/ErrorModal';
 import classes from './AddUser.module.css';
-import Wrapper from '../Helpers/Wrapper';
+
 
 const AddUser = (props) => {
   const [enteredUsername, setEnteredUsername] = useState('');
@@ -46,7 +46,7 @@ const AddUser = (props) => {
 
   return (
    
-    <Wrapper>
+    <>
       {error && (
         <ErrorModal
           title={error.title}
@@ -73,7 +73,7 @@ const AddUser = (props) => {
           <Button type="submit">Add User</Button>
         </form>
       </Card>
-      </Wrapper>
+      </>
   );
 };
 
