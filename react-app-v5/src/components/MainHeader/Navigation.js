4eda1a01230943095c87f4671087c66ac105183d
Mufil Rahman A <ramanizum@gmail.com>
2024-01-04 11:51:32 +0530

    use of useContext

--- react-app-v5/src/components/MainHeader/Navigation.js
+++ react-app-v5/src/components/MainHeader/Navigation.js
@@ -1,13 +1,12 @@
-import React from "react";
+import React, { useContext } from "react";
 
 import classes from "./Navigation.module.css";
 import AuthContext from "../../context/auth-context";
 
 const Navigation = (props) => {
+
+  const ctx = useContext(AuthContext)
   return (
-    <AuthContext.Consumer>
-      {(ctx) => {
-        return (
           <nav className={classes.nav}>
             <ul>
               {ctx.isLoggedIn && (
@@ -27,9 +26,6 @@ const Navigation = (props) => {
               )}
             </ul>
           </nav>
-        );
-      }}
-    </AuthContext.Consumer>
   );
 };
 
